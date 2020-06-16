4be18badc73d25dde1ed32921e973d1a9355e65a
Kishore Newton <kishorenke@gmail.com>
2020-06-16 17:03:00 +0530

    urls added

---
+++ index.js
@@ -0,0 +1,17 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details){
+        return {
+            cancel: true
+        }
+    },
+    // Urls you want to block
+    {
+        urls: [
+            "*://*.zedo.com/*",
+            "*://*.example.com/*",
+            "*://*.example2.com/*",
+            "*://*.ads.google.com/*"
+        ]
+    },
+    ["blocking"]
+)
